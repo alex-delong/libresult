9ecd6b1a1ac8a22151fb023d95398ecbce5a9a3e
Alex Delong <alexdelong243@gmail.com>
2023-04-27 11:05:20 -0700

    added namespaces to libraries

--- include/libexception.hpp
+++ include/libexception.hpp
@@ -1,19 +1,21 @@
 #include <cstring>
 #include <exception>
 
-class Exception : std::exception {
-  protected:
-    const char* message = "Exception";
-    char* location = nullptr;
-    char* message_location = nullptr;
-    void set_location(const char*);
-    Exception(const char*, const char*);
-  public:
-    Exception(const Exception&);
-    Exception(); 
-    Exception(const char*);
-    virtual const char* what() const noexcept override; 
-    const char* where() const;
-    Exception& operator=(const Exception& other);
-    virtual ~Exception(); 
-};
+namespace LibException {
+    class Exception : std::exception {
+      protected:
+        const char* message = "Exception";
+        char* location = nullptr;
+        char* message_location = nullptr;
+        void set_location(const char*);
+        Exception(const char*, const char*);
+      public:
+        Exception(const Exception&);
+        Exception(); 
+        Exception(const char*);
+        virtual const char* what() const noexcept override; 
+        const char* where() const;
+        Exception& operator=(const Exception& other);
+        virtual ~Exception(); 
+    };
+}
